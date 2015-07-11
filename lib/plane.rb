53c47812350e4d0c0a5e6823c12e478aba1005d7
sanjsanj <sanjsanj@hotmail.com>
2015-07-11 20:59:58 +0100

    first round of plane tests passed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,29 @@
 class Plane
+  def initialize
+    @location = 'air'
+  end
+
+  def flying?
+    @location == 'air'
+  end
+
+  def land
+    if @location == 'air'
+      @location = 'ground'
+    else
+      fail 'Can not land while landed'
+    end
+  end
+
+  def landed?
+    @location == 'ground'
+  end
+
+  def take_off
+    if @location == 'ground'
+      @location = 'air'
+    else
+      fail 'Can not take off if not landed'
+    end
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -15,15 +15,38 @@ require 'plane'
 # Are you testing that?
 
 describe Plane do
+  let(:plane) { Plane.new }
 
-  xit 'is flying when created'
+  it 'is flying when created' do
+    expect(plane).to be_flying
+  end
 
-  xit 'can land'
+  it 'can land' do
+    expect(plane).to respond_to :land
+  end
 
-  xit 'is landed after landing'
+  it 'is landed after landing' do
+    plane.land
+    expect(plane).to be_landed
+  end
 
-  xit 'can take off'
+  it 'can not land while landed' do
+    plane.land
+    expect { plane.land }.to raise_error 'Can not land while landed'
+  end
 
-  xit 'is flying after take off'
+  it 'can take off' do
+    expect(plane).to respond_to :take_off
+  end
+
+  it 'is flying after take off' do
+    plane.land
+    plane.take_off
+    expect(plane).to be_flying
+  end
+
+  it 'can not take off if not landed' do
+    expect { plane.take_off }.to raise_error 'Can not take off if not landed'
+  end
 
 end
